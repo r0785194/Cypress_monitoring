25aa9615f6f3cef3bf50460e0fff1c3fee4548df
Laura Nijsen <r0785194@ucll.be>
2024-05-31 15:15:59 +0200

    pipeline added telegraf and smoke job

--- .circleci/send_to_telegraf.sh
+++ .circleci/send_to_telegraf.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/xml" --data-binary "@target/surefire-reports/TEST-Setup.RunnerTest.xml" http://localhost:8080/telegraf
+curl -X POST -H "Content-Type: application/xml" --data-binary "/home/circleci/project/factory/container/test-results.xml" http://localhost:8080/telegraf
